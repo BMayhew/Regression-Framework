e742340762e65dc2f967df1b21a6180220581fa6
Butch Mayhew <bmayhew@daxko.com>
2017-09-27 10:31:41 -0500

    Updated URLs to new instance

--- lib/pages/edit_user_page.rb
+++ lib/pages/edit_user_page.rb
@@ -1,7 +1,7 @@
 class EditUserPage
   include PageObject
 
-  page_url 'http://ec2-54-191-181-182.us-west-2.compute.amazonaws.com/users/edit'
+  page_url 'http://ec2-54-190-54-0.us-west-2.compute.amazonaws.com//users/edit'
 
   link(:cancel_account, text: 'Cancel my account' )
 
--- lib/pages/home_page.rb
+++ lib/pages/home_page.rb
@@ -1,7 +1,7 @@
 class HomePage
   include PageObject
 
-  page_url "http://bit.ly/QAHomework"
+  page_url "http://bit.ly/QAHomework2"
 
   link(:my_account, text: 'My Account')
   div(:content_div, id: 'content')
