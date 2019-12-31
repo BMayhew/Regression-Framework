bf0851fbd181fcdd49b7256bfac83eaaa79eaab6
Butch Mayhew <bmayhew@daxko.com>
2019-12-30 18:45:36 -0600

    Testing 123

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,19 @@ require 'page-object/page_factory'
 require 'faker'
 require 'pages'
 
-if ENV['HEADLESS']
-  puts 'Running in headless mode'
-  browser = Watir::Browser.new :chrome, switches: %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate --disable-notifications --start-maximized --disable-gpu --headless --no-sandbox --disable-dev-shm-usage]
-  browser.driver.manage.timeouts.implicit_wait = 10 # seconds
-else
-  puts 'Running in non-headless mode'
-  browser = Watir::Browser.new :chrome
-end
+puts "Lets Do this"
+options = Selenium::WebDriver::Chrome::Options.new
+# options.add_argument('--start-fullscreen')
+browser = Watir::Browser.new(Selenium::WebDriver.for(:chrome, url: 'http://selenium__standalone-chrome-debug/wd/hub', options: options))
+
+# if ENV['HEADLESS']
+#   puts 'Running in headless mode'
+#   browser = Watir::Browser.new :chrome, switches: %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate --disable-notifications --start-maximized --disable-gpu --headless --no-sandbox --disable-dev-shm-usage]
+#   browser.driver.manage.timeouts.implicit_wait = 10 # seconds
+# else
+#   puts 'Running in non-headless mode'
+#   browser = Watir::Browser.new :chrome
+# end
 
 RSpec.configure do |config|
   config.include PageObject::PageFactory
